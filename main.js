323e79a8bdf336de02b9363202c65f68a21204ea
Dennis Kehrig <mailgithub@denniskehrig.de>
2013-02-25 14:15:41 +0100

    Whitespace highlighting now uses an overlay mode, which restores compatibility with CodeMirror 3. Many thanks to Lance Campbell (@lkcampbell) for getting this started on the Brackets mailing list and getting the necessary hint from the CodeMirror team (see https://github.com/marijnh/CodeMirror/pull/1281/commits)!

--- main.js
+++ main.js
@@ -25,280 +25,174 @@
 /*jslint vars: true, plusplus: true, devel: true, regexp: true, nomen: true, indent: 4, maxerr: 50 */
 /*global define, $, brackets, Node, CodeMirror */
 
-
 define(function (require, exports, module) {
-	"use strict";
-	
-	if (CodeMirror.version && CodeMirror.version.slice(0, 1) !== "2") {
-		console.log("[ShowWhitespace] CodeMirror 2 required, you are using " + CodeMirror.version);
-		return;
-	}
+    "use strict";
+    
+    
+    // --- Required modules ---
 
-	
-	// --- Required modules ---
+    var CommandManager     = brackets.getModule("command/CommandManager");
+    var DocumentManager    = brackets.getModule("document/DocumentManager");
+    var EditorManager      = brackets.getModule("editor/EditorManager");
+    var ExtensionUtils     = brackets.getModule("utils/ExtensionUtils");
+    var Menus              = brackets.getModule("command/Menus");
+    var PreferencesManager = brackets.getModule("preferences/PreferencesManager");
 
-	var Commands           = brackets.getModule("command/Commands");
-	var CommandManager     = brackets.getModule("command/CommandManager");
-	var DocumentManager    = brackets.getModule("document/DocumentManager");
-	var EditorManager      = brackets.getModule("editor/EditorManager");
-	var Menus              = brackets.getModule("command/Menus");
-	var PreferencesManager = brackets.getModule("preferences/PreferencesManager");
-	var ExtensionUtils     = brackets.getModule("utils/ExtensionUtils");
+    
+    // --- Settings ---
+    
+    var commandId          = "denniskehrig.ShowWhitespace.toggle";
+    var preferencesId      = "denniskehrig.ShowWhitespace";
+    var defaultPreferences = { checked: false };
 
-	
-	// --- Settings ---
-	
-	var preferencesId      = "denniskehrig.ShowWhitespace";
-	var defaultPreferences = { checked: false };
-	var commandId          = "denniskehrig.ShowWhitespace.toggle";
+    
+    // --- State Variables ---
+    
+    var _preferences;
+    var _command;
+    var _styleTag;
+    
+    var _appendSpace = false;
+    
+    
+    // --- Functionality ---
 
-	
-	// --- State Variables ---
-	
-	var _preferences;
-	var _command;
-	var _$styleTag;
+    var overlay = {
+        token: function (stream, state) {
+            var ch,
+                ateCode = false;
+            
+            // Boolean makes JSLint happy
+            while (Boolean(ch = stream.peek())) {
+                if (ch === " " || ch === "\t") {
+                    if (ateCode) {
+                        // Return now to mark all code seen so far as not necessary to highlight
+                        return null;
+                    }
+                    // Eat the whitespace
+                    stream.next();
+                    // CodeMirror merges consecutive tokens with the same style
+                    // To prevent that we simply add a space to the style every other time
+                    // This disables CodeMirror's string comparison while having no effect on the CSS class
+                    _appendSpace = !_appendSpace;
+                    return "dk-whitespace-" + (ch === " " ? "space" : "tab") + (_appendSpace ? " " : "");
+                } else {
+                    stream.next();
+                    ateCode = true;
+                }
+            }
+            return null;
+        }
+    };
+        
+    function refreshCodeMirror() {
+        var fullEditor = EditorManager.getCurrentFullEditor();
+        if (!fullEditor || !fullEditor._codeMirror) { return; }
 
-	var _Line;
-	var _LineGetHTML;
-	
+        fullEditor._codeMirror.removeOverlay(overlay);
+        if (_command.getChecked()) {
+            fullEditor._codeMirror.addOverlay(overlay);
+        }
+    }
 
-	// --- Event Handlers ---
+    // --- Event Handlers ---
 
-	function onCommandExecuted() {
-		if (! _command.getChecked()) {
-			_command.setChecked(true);
-		} else {
-			_command.setChecked(false);
-		}
-	}
+    function onCommandExecuted() {
+        if (!_command.getChecked()) {
+            _command.setChecked(true);
+        } else {
+            _command.setChecked(false);
+        }
+    }
 
-	function onCheckedStateChange() {
-		_preferences.setValue("checked", Boolean(_command.getChecked()));
-		refreshCodeMirror();
-	}
+    function onCheckedStateChange() {
+        _preferences.setValue("checked", Boolean(_command.getChecked()));
+        refreshCodeMirror();
+    }
 
-	// Functionality
+    // --- Loaders and Unloaders ---
 
-	function refreshCodeMirror() {
-		var fullEditor = EditorManager.getCurrentFullEditor();
-		if (! fullEditor || ! fullEditor._codeMirror) { return; }
-
-		if (_command.getChecked()) {
-			patchCodeMirror(fullEditor._codeMirror);
-		} else {
-			unpatchCodeMirror();
-		}
-
-		fullEditor._codeMirror.refresh();
-		$.each(EditorManager.getInlineEditors(fullEditor), function (index, inlineEditor) {
-			inlineEditor._codeMirror.refresh();
-		});
-	}
-
-	function patchCodeMirror(codeMirror) {
-		// Avoid double patching
-		if (_Line && _LineGetHTML) { return; }
-
-		// Remember Line and Line.getHTML to be able to unpatch without a reference to CM
-		_Line = Object.getPrototypeOf(codeMirror.getLineHandle(0));
-		_LineGetHTML = _Line.getHTML;
-
-		// Constants
-		var TAG_CLOSE     = "</span>";
-		var CM_TAB        = "<span class=\"cm-tab\">";
-		var TAB_NORMAL    = "<span class=\"cm-tab dk-normal\">";
-		var TAB_LEADING   = "<span class=\"cm-tab dk-leading\">";
-		var SPACE_NORMAL  = "<span class=\"cm-space dk-normal\">"
-		var SPACE_LEADING = "<span class=\"cm-space dk-leading\">";
-
-		// Closure to make our getHTML independent of this extension
-		var _super = _Line.getHTML;
-		_Line.getHTML = function getHTML(makeTab, wrapAt, wrapId, wrapWBR) {
-			var html = _super.apply(this, arguments);
-			
-			// Nothing to do
-			if (! _command || ! _command.getChecked() || ! this.text) { return html; }
-			
-			// Local variables for the loop
-			var pos, part;
-			
-			// Optimizations
-			var length = html.length;
-
-			// Output
-			var output = [];
-			
-			// State
-			var offset = 0;
-			var tags   = [];
-			
-			var leading   = true;
-			var spaceOpen = SPACE_LEADING;
-			var tabOpen   = TAB_LEADING;
-
-			while (offset < length) {
-				// Tag mode
-				if (html.slice(offset, offset + 1) === "<"){
-					// Look for the end of the tag
-					pos  = html.indexOf(">", offset + 1);
-					part = html.slice(offset, pos + 1);
-					
-					// Update the state
-					offset = pos + 1;
-					if (part.slice(1, 2) === "/") {
-						tags.pop();
-					} else {
-						tags.push(part);
-					}
-
-					// Inject the leading class if necessary
-					if (part === CM_TAB) { part = tabOpen; }
-				}
-				// Text mode
-				else {
-					// Look for the start of a tag
-					pos = html.indexOf("<", offset);
-					// The entire rest of the string is escaped text
-					if (pos === -1) { pos = length + 1; }
-					part = html.slice(offset, pos);
-					
-					// Update the state
-					offset = pos;
-
-					// No need to handle empty strings
-					if (part === "") { continue; }
-
-					// Leave the spaces in tabs as they are
-					if (tags[tags.length - 1] !== CM_TAB) {
-						// Find out if the indentation ends in this part
-						if (leading) {
-							// Consume indentation spaces
-							for (pos = 0; pos < part.length && part.slice(pos, pos + 1) === " "; pos++) {
-								output.push(spaceOpen, " ", TAG_CLOSE);
-							}
-							// The part contains non-space characters: end of indentation
-							if (pos < part.length) {
-								leading = false;
-							}
-							// Remove the spaces we have consumed
-							part = part.slice(pos);
-							// The rest of the part will be handled further below
-
-							// From now on, don't set the leading class in the HTML output
-							if (! leading) {
-								spaceOpen = SPACE_NORMAL;
-								tabOpen   = TAB_NORMAL;
-							}
-						}
-						
-						// Mark the spaces appropriately
-						part = part.replace(/ /g, spaceOpen + " " + TAG_CLOSE);
-					}
-				}
-				
-				// Add the part to the output
-				output.push(part);
-			}
-
-			output = output.join("");
-			return output;
-		};
-	}
-
-	function unpatchCodeMirror() {
-		if (_Line && _LineGetHTML) {
-			_Line.getHTML = _LineGetHTML;
-			_LineGetHTML = null;
-			_Line = null;
-		}
-	}
-
-	
-	// --- Loaders and Unloaders ---
-
-	function loadPreferences() {
-		_preferences = PreferencesManager.getPreferenceStorage(preferencesId, defaultPreferences);
-	}
+    function loadPreferences() {
+        _preferences = PreferencesManager.getPreferenceStorage(preferencesId, defaultPreferences);
+    }
 
 
-	function loadStyle() {
-		ExtensionUtils.loadStyleSheet(module, "main.less").done(function ($node) {
-			_$styleTag = $node;
-		});
-	}
+    function loadStyle() {
+        ExtensionUtils.loadStyleSheet(module, "main.less").done(function (node) {
+            _styleTag = node;
+        });
+    }
 
-	function unloadStyle() {
-		_$styleTag.remove();
-	}
+    function unloadStyle() {
+        $(_styleTag).remove();
+    }
 
-	
-	function loadCommand() {
-		_command = CommandManager.get(commandId);
-		
-		if (! _command) {
-			_command = CommandManager.register("Show Whitespace", commandId, onCommandExecuted);
-		} else {
-			_command._commandFn = onCommandExecuted;
-		}
+    
+    function loadCommand() {
+        _command = CommandManager.get(commandId);
+        
+        if (!_command) {
+            _command = CommandManager.register("Show Whitespace", commandId, onCommandExecuted);
+        } else {
+            _command._commandFn = onCommandExecuted;
+        }
 
-		$(_command).on("checkedStateChange", onCheckedStateChange);
-		
-		// Apply preferences
-		_command.setChecked(_preferences.getValue("checked"));
-	}
+        $(_command).on("checkedStateChange", onCheckedStateChange);
+        
+        // Apply preferences
+        _command.setChecked(_preferences.getValue("checked"));
+    }
 
-	function unloadCommand() {
-		_command.setChecked(false);
-		$(_command).off("checkedStateChange", onCheckedStateChange);
-		_command._commandFn = null;
-	}
+    function unloadCommand() {
+        _command.setChecked(false);
+        $(_command).off("checkedStateChange", onCheckedStateChange);
+        _command._commandFn = null;
+    }
 
-	
-	function loadMenuItem() {
-		Menus.getMenu("view-menu").addMenuItem(commandId, "Ctrl-Alt-W");
-	}
+    
+    function loadMenuItem() {
+        Menus.getMenu("view-menu").addMenuItem(commandId, "Ctrl-Alt-W");
+    }
 
-	function unloadMenuItem() {
-		// Not implemented
-		// Menus.getMenu("view-menu").removeMenuItem(commandId);
-	}
-	
-	
-	function loadDocumentSync() {
-		$(DocumentManager).on("currentDocumentChange", refreshCodeMirror);
-	}
+    function unloadMenuItem() {
+        Menus.getMenu("view-menu").removeMenuItem(commandId);
+    }
+    
+    
+    function loadDocumentSync() {
+        $(DocumentManager).on("currentDocumentChange", refreshCodeMirror);
+    }
 
-	function unloadDocumentSync() {
-		$(DocumentManager).off("currentDocumentChange", refreshCodeMirror);
-	}
+    function unloadDocumentSync() {
+        $(DocumentManager).off("currentDocumentChange", refreshCodeMirror);
+    }
 
-	
-	// Setup the UI
-	function load() {
-		loadPreferences();
-		loadStyle();
-		loadCommand();
-		loadMenuItem();
-		loadDocumentSync();
-	}
+    
+    // Setup the UI
+    function load() {
+        loadPreferences();
+        loadStyle();
+        loadCommand();
+        loadMenuItem();
+        loadDocumentSync();
+    }
 
-	// Tear down the UI
-	function unload() {
-		unloadDocumentSync();
-		unloadMenuItem();
-		unloadCommand();
-		unloadStyle();
-	}
+    // Tear down the UI
+    function unload() {
+        unloadDocumentSync();
+        unloadMenuItem();
+        unloadCommand();
+        unloadStyle();
+    }
 
 
-	// --- Exports ---
-	
-	exports.load = load;
-	exports.unload = unload;
+    // --- Exports ---
+    
+    exports.load = load;
+    exports.unload = unload;
 
-	
-	// --- Initializiation ---
-	
-	load();
+    
+    // --- Initializiation ---
+    
+    load();
 });
