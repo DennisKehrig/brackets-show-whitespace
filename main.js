e6e1b8cac6bbf65dcdf279d178e69b04e5224875
Dennis Kehrig <mailgithub@denniskehrig.de>
2013-02-25 17:01:03 +0100

    Restored compatibility with CodeMirror 2, which doesn't have addOverlay

--- main.js
+++ main.js
@@ -51,7 +51,13 @@ define(function (require, exports, module) {
     var _preferences;
     var _command;
     var _styleTag;
-    
+
+    // CodeMirror 2 doesn't have addOverlay, and doesn't set a version either.
+    var _useLegacyVersion = !CodeMirror.version;
+    var _Line;
+    var _LineGetHTML;
+
+    // State for the overlay mode, which is supposed to be stateless and so can't use the state functionality provided by CodeMirror
     var _appendSpace = false;
     
     
@@ -72,7 +78,8 @@ define(function (require, exports, module) {
                     // Eat the whitespace
                     stream.next();
                     // CodeMirror merges consecutive tokens with the same style
-                    // To prevent that we simply add a space to the style every other time
+                    // There's a setting called "flattenSpans" to prevent that, but it's for the whole editor
+                    // So instead we simply append a space character to the style every other time
                     // This disables CodeMirror's string comparison while having no effect on the CSS class
                     _appendSpace = !_appendSpace;
                     return "dk-whitespace-" + (ch === " " ? "space" : "tab") + (_appendSpace ? " " : "");
@@ -85,16 +92,120 @@ define(function (require, exports, module) {
         }
     };
         
+    function patchCodeMirror(codeMirror) {
+        // Avoid double patching
+        if (_Line && _LineGetHTML) { return; }
+
+        // Remember Line and Line.getHTML to be able to unpatch without a reference to CM
+        _Line = Object.getPrototypeOf(codeMirror.getLineHandle(0));
+        _LineGetHTML = _Line.getHTML;
+
+        // Constants
+        var TAG_CLOSE = "</span>";
+        var CM_TAB    = "<span class=\"cm-tab\">";
+        var TAB       = "<span class=\"cm-tab cm-dk-whitespace-tab\">";
+        var SPACE     = "<span class=\"cm-space cm-dk-whitespace-space\">";
+
+        // Closure to make our getHTML independent of this extension
+        var _super = _Line.getHTML;
+        _Line.getHTML = function getHTML(makeTab, wrapAt, wrapId, wrapWBR) {
+            var html = _super.apply(this, arguments);
+            
+            // Nothing to do
+            if (!_command || !_command.getChecked() || !this.text) { return html; }
+            
+            // Local variables for the loop
+            var pos, part;
+            
+            // Optimizations
+            var length = html.length;
+            
+            // Output
+            var output = [];
+            
+            // State
+            var offset = 0;
+            var tags   = [];
+            
+            while (offset < length) {
+                if (html.slice(offset, offset + 1) === "<") {
+                    // Tag mode
+                    
+                    // Look for the end of the tag
+                    pos  = html.indexOf(">", offset + 1);
+                    part = html.slice(offset, pos + 1);
+                    
+                    // Update the state
+                    offset = pos + 1;
+                    if (part.slice(1, 2) === "/") {
+                        tags.pop();
+                    } else {
+                        tags.push(part);
+                    }
+                    
+                    if (part === CM_TAB) { part = TAB; }
+                } else {
+                    // Text mode
+                    
+                    // Look for the start of a tag
+                    pos = html.indexOf("<", offset);
+                    // The entire rest of the string is escaped text
+                    if (pos === -1) { pos = length + 1; }
+                    part = html.slice(offset, pos);
+                    
+                    // Update the state
+                    offset = pos;
+                    
+                    // Leave the spaces in tabs as they are
+                    if (tags[tags.length - 1] !== CM_TAB) {
+                        // Mark the spaces appropriately
+                        part = part.replace(/ /g, SPACE + " " + TAG_CLOSE);
+                    }
+                }
+                
+                // No need to handle empty strings
+                if (part !== "") {
+                    // Add the part to the output
+                    output.push(part);
+                }
+            }
+            
+            output = output.join("");
+            return output;
+        };
+    }
+
+    function unpatchCodeMirror() {
+        if (_Line && _LineGetHTML) {
+            _Line.getHTML = _LineGetHTML;
+            _LineGetHTML = null;
+            _Line = null;
+        }
+    }
+
     function refreshCodeMirror() {
         var fullEditor = EditorManager.getCurrentFullEditor();
         if (!fullEditor || !fullEditor._codeMirror) { return; }
 
-        fullEditor._codeMirror.removeOverlay(overlay);
-        if (_command.getChecked()) {
-            fullEditor._codeMirror.addOverlay(overlay);
+        if (_useLegacyVersion) {
+            if (_command.getChecked()) {
+                patchCodeMirror(fullEditor._codeMirror);
+            } else {
+                unpatchCodeMirror();
+            }
+            
+            fullEditor._codeMirror.refresh();
+            $.each(EditorManager.getInlineEditors(fullEditor), function (index, inlineEditor) {
+                inlineEditor._codeMirror.refresh();
+            });
+        } else {
+            fullEditor._codeMirror.removeOverlay(overlay);
+            if (_command.getChecked()) {
+                fullEditor._codeMirror.addOverlay(overlay);
+            }
         }
     }
-
+    
     // --- Event Handlers ---
 
     function onCommandExecuted() {
